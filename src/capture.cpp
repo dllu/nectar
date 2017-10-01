12bb8a5102a9ba6b75bd62555d82302f0148e9f0
Daniel Lu <daniel@lawrence.lu>
2017-10-01 15:20:30 -0700

    switch to raw format, add histogram, fix crashes

--- src/capture.cpp
+++ src/capture.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <iomanip>
 #include <sstream>
+#include <fstream>
 
 using namespace CAlkUSB3;
 using namespace std;
@@ -9,7 +10,7 @@ using namespace std;
 const int rows = 2048;
 const int cols = 4096;
 
-void capture(INectaCamera &cam, int shutterspeed, int analog_gain, int cds_gain, int gamma) {
+void capture(INectaCamera &cam, int shutterspeed, int analog_gain, int cds_gain) {
     // Get list of connected cameras
     Array<String> camList = cam.GetCameraList();
     if (camList.Size() == 0) {
@@ -20,24 +21,34 @@ void capture(INectaCamera &cam, int shutterspeed, int analog_gain, int cds_gain,
     cam.SetCamera(0);
     cam.Init();
 
+    cam.SetADCResolution(12);
+    cerr << (int)cam.GetMaxADCResolution() << endl;
+    cam.SetVideoMode(1);
+    cam.SetColorCoding(ColorCoding::Raw16);
     cam.SetImageSizeX(cols);
     cam.SetImageSizeY(rows);
     cam.SetGain(analog_gain);
     cam.SetCDSGain(cds_gain);
     cam.SetShutter(shutterspeed);
-    cam.SetGamma(gamma / 1000.0);
+    cam.SetLinePeriod(cam.GetMinLinePeriod());
+    cam.SetPacketSize(min(16384U, cam.GetMaxPacketSize()));
 
     cout << "Starting acquisition..." << endl;
     // Start acquisition
 
-    int nOfSamples = 300;
+    int n = 1000;
 
     cam.SetAcquire(true);
-    for (int f = 0; f < nOfSamples; f++) {
+    for (int f = 0; f < n; f++) {
         // Get new frame
         stringstream ss;
-        ss << "im" << setfill('0') << setw(4) << f << ".bmp";
-        cam.GetImagePtr(true).Save(ss.str().c_str());
+        ss << "im" << setfill('0') << setw(4) << f << ".bin";
+        // cam.GetImagePtr(true).Save(ss.str().c_str());
+        auto raw_image = cam.GetRawData();
+        ofstream fout;
+        fout.open(ss.str(), ios::out | ios::binary);
+        fout.write((char*)raw_image.Data(), raw_image.Size());
+        fout.close();
     }
 
     // Stop acquisition
@@ -48,18 +59,17 @@ void capture(INectaCamera &cam, int shutterspeed, int analog_gain, int cds_gain,
 int main(int argc, char *argv[]) {
     INectaCamera &cam = INectaCamera::Create();
     if (argc < 5) {
-        cerr << "USAGE: capture shutterspeed analog_gain cds_gain gamma" << endl;
+        cerr << "USAGE: capture analog_gain cds_gain shutterspeed" << endl;
         return 1;
     }
 
     int analog_gain = atoi(argv[1]);
     int cds_gain = atoi(argv[2]);
     int shutterspeed = atoi(argv[3]);
-    int gamma = atoi(argv[4]);
 
     // capture
     try {
-        capture(cam, shutterspeed, analog_gain, cds_gain, gamma);
+        capture(cam, shutterspeed, analog_gain, cds_gain);
     } catch (const Exception &ex) {
         cout << "Exception " << ex.Name() << " occurred" << endl
             << ex.Message() << endl;
--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,15 @@ void printColorCodings(INectaCamera &cam) {
     }
 }
 
-void capture(INectaCamera &cam) {
+void printVideoModes(INectaCamera &cam) {
+    auto acc = cam.GetAvailableVideoModes();
+    for (auto cc = 0; cc < acc.Size(); cc++) {
+        cerr << (int)(acc[cc].GetVideoMode()) << endl;
+    }
+    cerr << "selected: " << (int) cam.GetVideoMode() << endl;
+}
+
+void capture(INectaCamera &cam, int *gain, int *cds_gain, int *shutter) {
     // Get list of connected cameras
     Array<String> camList = cam.GetCameraList();
     if (camList.Size() == 0) {
@@ -32,39 +40,84 @@ void capture(INectaCamera &cam) {
     cam.SetCamera(0);
     cam.Init();
 
+    cam.SetADCResolution(12);
+    cam.SetVideoMode(1);
+    cam.SetColorCoding(ColorCoding::Raw16);
     cam.SetImageSizeX(cols);
     cam.SetImageSizeY(rows);
+
+    cam.SetPacketSize(min(16384U, cam.GetMaxPacketSize()));
 
     cerr  << "Starting acquisition..." << endl;
     // Start acquisition
 
-    int nOfSamples = 10000;
 
-    cv::Mat cv_image(rows, cols, CV_8UC3, cv::Scalar(0, 0, 0));
+    cam.SetAcquire(true);
+    cv::Mat cv_image(rows, cols / 2, CV_8UC3, cv::Scalar(0, 0, 0));
+
+    std::vector<int>reds(256, 0);
+    std::vector<int>greens(256, 0);
+    std::vector<int>blues(256, 0);
     for (int f = 0; true; f++) {
         if (cv::getWindowProperty("necta", 0) < 0) {
-            cout
-                << cv::getTrackbarPos("analog gain", "necta") << " "
-                << cv::getTrackbarPos("cds gain", "necta") << " "
-                << cv::getTrackbarPos("shutterspeed", "necta") << " "
-                << cv::getTrackbarPos("gamma", "necta") / 1000.0 << endl;
             break;
         }
-        cam.SetGain(cv::getTrackbarPos("analog gain", "necta"));
-        cam.SetCDSGain(cv::getTrackbarPos("cds gain", "necta"));
-        cam.SetShutter(cv::getTrackbarPos("shutterspeed", "necta"));
-        cam.SetGamma(cv::getTrackbarPos("gamma", "necta") / 1000.0);
-        cam.SetAcquire(true);
+        std::fill(reds.begin(), reds.end(), 0);
+        std::fill(greens.begin(), greens.end(), 0);
+        std::fill(blues.begin(), blues.end(), 0);
+        cam.SetGain(*gain);
+        cam.SetCDSGain(*cds_gain);
+        cam.SetShutter(*shutter);
+
         // Get new frame
-        stringstream ss;
-        ss << "im" << setfill('0') << setw(4) << f << ".bmp";
-        auto raw_image = cam.GetImageData();
-        cam.SetAcquire(false);
-        cv_image.data = raw_image.Data();
-        cv::imshow("necta", cv_image.clone());
+        auto raw_image = cam.GetRawData();
+        for (int i = 0; i < rows/2; i++) {
+            for (int j = 0; j < cols/2; j++) {
+                cv::Vec3b &intensity = cv_image.at<cv::Vec3b>(i, j);
+
+                intensity.val[0] = raw_image[2 * (2 * i * cols + (2 * j + 1)) + 1];
+                intensity.val[1] = (raw_image[2 * ((2 * i + 1) * cols + (2 * j + 1)) + 1] + raw_image[2 * (2 * i * cols + 2 * j) + 1]) / 2;
+                intensity.val[2] = raw_image[2 * ((2 * i + 1) * cols + 2 * j) + 1];
+
+                reds[intensity.val[0]]++;
+                greens[intensity.val[1]]++;
+                blues[intensity.val[2]]++;
+            }
+        }
+        for (int i = 0; i < rows/2; i++) {
+            for (int j = 0; j < 256; j++) {
+                cv::Vec3b &intensity = cv_image.at<cv::Vec3b>(rows - i - 1, j);
+                if (i * 100 < reds[j]) {
+                    intensity.val[0] = 255;
+                } else {
+                    intensity.val[0] = 0;
+                }
+                if (i * 100 < greens[j]) {
+                    intensity.val[1] = 255;
+                } else {
+                    intensity.val[1] = 0;
+                }
+                if (i * 100 < blues[j]) {
+                    intensity.val[2] = 255;
+                } else {
+                    intensity.val[2] = 0;
+                }
+            }
+        }
+        if (cv::getWindowProperty("necta", 0) < 0) {
+            break;
+        }
+        cv::imshow("necta", cv_image);
+
+        // stringstream ss;
+        // ss << "im" << setfill('0') << setw(4) << f << ".bmp";
         // cam.GetImagePtr(true).Save(ss.str().c_str());
+        if (cv::getWindowProperty("necta", 0) < 0) {
+            break;
+        }
         cv::waitKey(1);
     }
+    cam.SetAcquire(false);
 
     cerr << "Acquisition stopped" << endl;
 }
@@ -76,24 +129,20 @@ int main(int argc, char *argv[]) {
     int analog_gain = 10;
     int cds_gain = 1;
     int shutterspeed = 2000;
-    int gamma = 1000;
 
     cv::createTrackbar("shutterspeed", "necta", &shutterspeed, 10000);
     cv::createTrackbar("analog gain", "necta", &analog_gain, 20);
     cv::createTrackbar("cds gain", "necta", &cds_gain, 1);
-    cv::createTrackbar("gamma", "necta", &gamma, 2000);
 
     // capture
     try {
-        capture(cam);
+        capture(cam, &analog_gain, &cds_gain, &shutterspeed);
     } catch (const Exception &ex) {
         cerr << "Exception " << ex.Name() << " occurred" << endl
             << ex.Message() << endl;
+    } catch(...) {
+        cerr << "Unhandled excpetion" << endl;
     }
-    cout
-        << cv::getTrackbarPos("analog gain", "necta") << " "
-        << cv::getTrackbarPos("cds gain", "necta") << " "
-        << cv::getTrackbarPos("shutterspeed", "necta") << " "
-        << cv::getTrackbarPos("gamma", "necta") / 1000.0 << endl;
+    cout << analog_gain << " " << cds_gain << " " << shutterspeed << endl;
     return 0;
 }
