81b0499fd152828740868eeb711a69a85c3e00aa
Daniel Lu <daniel@lawrence.lu>
2017-10-01 15:20:46 -0700

    add a matlab script to preview raw format

---
+++ matlab/previewbin.m
@@ -0,0 +1,14 @@
+close all;
+a = [];
+for ind=0:4
+    f = fopen(sprintf('im%04d.bin', ind));
+    a = [a; fread(f, 16777216)];
+    fclose(f);
+end
+aa = reshape(a(2:2:end), 4096, []) * 256 + reshape(a(1:2:end), 4096, []);
+b = aa(2:2:end,1:2:end);
+g = (aa(2:2:end,2:2:end) + aa(1:2:end,1:2:end)) / 2;
+r = aa(1:2:end,2:2:end);
+%imshow(aa' / max(aa(:)));
+rgb = cat(3, r, g, b);
+imshow(rgb / max(rgb(:)));
