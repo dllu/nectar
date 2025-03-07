99348b0b732f7b4db2ac5d45b1c442b894817ce5
Daniel Lu <daniel@lawrence.lu>
2025-03-06 20:06:04 -0800

    cross correlation experimentation...

--- python/preview.py
+++ python/preview.py
@@ -4,7 +4,7 @@ import numpy as np
 from pathlib import Path
 import time
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 import scipy
 
@@ -14,21 +14,40 @@ linescans = Path("/mnt/data14/pictures/linescan")
 preview = "preview_raw.png"
 
 
+def subpixel_peak(y, sigma, initial):
+    peak = initial
+    x = np.arange(len(y))
+    sigma2 = sigma**2
+    for iter in range(10):
+        weights = np.exp(-((x - peak) ** 2) / sigma2) * y
+        peak = np.sum(weights * x) / np.sum(weights)
+    return peak
+
+
 def windowed_cross_correlation(
     green_1: np.ndarray,
     green_2: np.ndarray,
-    window_size: int = 256,
-    corr_size: int = 11,
+    window_step: int = 512,
+    window_size: int = 2048,
+    corr_size: int = 31,
 ) -> np.ndarray:
     """
     green_1: (h, w) where w >> h and w >> window_size
     green_2: (h, w)
     """
 
-    xs = np.arange(0, green_1.shape[1], window_size)
+    # green_1 = green_1[1224:, :]
+    # green_2 = green_2[1224:, :]
+    xs = np.arange(0, green_1.shape[1] - window_size, window_step)
     ys = []
+    refined_ys = []
+    good_xs = []
     corr_half = corr_size // 2
-    for x in xs:
+
+    heatmap = np.zeros((len(xs), corr_size))
+    print(heatmap.shape)
+
+    for xi, x in enumerate(xs):
         window_2 = green_2[:, x + corr_half : x + window_size - corr_half]
 
         corrs = np.zeros((corr_size,))
@@ -38,21 +57,48 @@ def windowed_cross_correlation(
                 :,
                 x + corr_half + corr_x : x + window_size - corr_half + corr_x,
             ]
-            corr_value = np.sum(window_1 * window_2)
+
+            # corr_value = np.sum(window_1 * window_2)
+            error = np.sum(np.abs(window_1 - window_2))
+            corr_value = -error
+            # corr_value = -np.log(error)
+
             corrs[corr_ind] = corr_value
 
         peak_idx = np.argmax(corrs)
-        x = np.array([peak_idx - 1, peak_idx, peak_idx + 1])
-        y = corrs[peak_idx - 1 : peak_idx + 2]
-        coeffs = np.polyfit(x, y, 2)
+
+        if peak_idx <= 0 or peak_idx >= corr_size - 1:
+            continue
+
+        # polyx = np.array([peak_idx - 1, peak_idx, peak_idx + 1])
+        # polyy = corrs[polyx]
+        # coeffs = np.polyfit(polyx, polyy, 2)
 
         # The vertex of the parabola (peak of the quadratic fit) is at -b/(2a)
-        refined_peak_idx = -coeffs[1] / (2 * coeffs[0])
+        # refined_peak_idx = -coeffs[1] / (2 * coeffs[0])
+        ncorrs = corrs - np.min(corrs)
+        ncorrs = ncorrs / np.max(ncorrs)
+        refined_peak_idx = subpixel_peak(ncorrs, 1.5, peak_idx)
 
-        ys.append(refined_peak_idx - corr_half)
+        # plt.plot(ncorrs, 'b+')
+        # plt.plot([refined_peak_idx, refined_peak_idx], [0, 1], 'k--')
+        # plt.show()
 
-    # plt.plot(xs, ys)
-    # plt.show()
+        # corrs -= np.min(corrs)
+        # corrs /= np.max(corrs)
+        heatmap[xi, :] = corrs
+
+        ys.append(peak_idx - corr_half)
+        refined_ys.append(refined_peak_idx - corr_half)
+        good_xs.append(x)
+    plt.show()
+
+    plt.imshow(heatmap)
+    plt.show()
+    plt.plot(good_xs, ys, "b.")
+    plt.plot(good_xs, refined_ys, "r.")
+    plt.plot(xs, np.zeros_like(xs), "k--")
+    plt.show()
 
 
 def bin_to_rgb(data: np.ndarray) -> np.ndarray:
@@ -69,7 +115,7 @@ def bin_to_rgb(data: np.ndarray) -> np.ndarray:
 
     raw_blue = data[1::2, 0::2]
 
-    # windowed_cross_correlation(raw_green_1, raw_green_2)
+    windowed_cross_correlation(raw_green_1, raw_green_2)
 
     raw_green = (raw_green_1 + raw_green_2) / 2
 
@@ -211,7 +257,7 @@ def patch_denoise(
     return denoised
 
 
-def process_preview(g: Path, padding: int = 5, max_chunks: int = 96):
+def process_preview(g: Path, padding: int = 5, max_chunks: int = 192):
     bins = sorted(list(g.glob("*.bin")))
 
     start = np.inf
@@ -238,7 +284,7 @@ def process_preview(g: Path, padding: int = 5, max_chunks: int = 96):
         grad_y, grad_x = np.gradient(raw_green)
         magnitude = np.sqrt(grad_x**2 + grad_y**2) + 0.1 * max_green
         energy = np.abs(grad_x) / magnitude
-        cv2.imwrite(f"/home/dllu/test/{i:05d}.png", energy * 255)
+        # cv2.imwrite(f"/home/dllu/test/{i:05d}.png", energy * 255)
         score = np.percentile(energy, 99)
         scores.append(score)
 
@@ -278,7 +324,10 @@ def process_preview(g: Path, padding: int = 5, max_chunks: int = 96):
 
 
 def main():
-    for g in sorted(list(linescans.glob("20*"))):
+    for g in sorted(list(linescans.glob("2024-09-10-06-00-05*"))): # yamanote
+    # for g in sorted(list(linescans.glob("2024-09-19-03-33-57*"))): # fuxing
+    # for g in sorted(list(linescans.glob("2024-09-14-02-13-13*"))): # hktram
+    # for g in sorted(list(linescans.glob("nyc4"))):
         if not g.is_dir():
             continue
 
@@ -289,9 +338,11 @@ def main():
             process_preview(g)
         except KeyboardInterrupt:
             break
+        """
         except Exception as e:
             print(f"oh no! {g} {e}")
             continue  # anyway
+        """
 
 
 if __name__ == "__main__":
